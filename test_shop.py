ef9a6da24ca7fa2e1f09135483b74b77d44c1d50
Natasha Garner <natasha_garner@hotmail.co.uk>
2023-03-31 11:21:03 +0100

    commit file

---
+++ test_shop.py
@@ -0,0 +1,10 @@
+import unittest
+from shop import shopping
+
+class MyTestCase(unittest.shopping):
+    def test_shopping():
+        assert shopping() is None  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
